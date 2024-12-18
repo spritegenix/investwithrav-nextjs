5acb006d5e714ca483d4c724f6857e380ee027bf
Ashik <ashikshettyc@gmail.com>
2024-12-18 20:09:53 +0530

    gallery

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eadeabea45eda448"; }
+/******/ 		__webpack_require__.h = function() { return "c3f89a43de457d0c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/gallery.js
+++ pages/gallery.js
@@ -2,44 +2,36 @@ import Breadcumb from '@/src/components/Breadcumb';
 import Layout from '@/src/layout/Layout';
 
 const Gallery = () => {
+  const galleryImages = [
+    '../assets/images/home/gallery/01.jpg',
+    '../assets/images/home/gallery/02.jpg',
+    '../assets/images/home/gallery/03.jpg',
+    '../assets/images/home/gallery/04.jpg',
+    '../assets/images/home/gallery/05.jpg',
+    '../assets/images/home/gallery/06.jpg',
+    '../assets/images/home/gallery/07.jpg',
+    '../assets/images/home/gallery/08.jpg',
+    '../assets/images/home/gallery/09.jpg',
+    '../assets/images/home/gallery/10.jpg',
+    '../assets/images/home/gallery/11.webp',
+    '../assets/images/home/gallery/12.jpeg',
+    '../assets/images/home/gallery/13.jpeg',
+    '../assets/images/home/gallery/14.jpeg',
+    '../assets/images/home/gallery/15.jpeg',
+    '../assets/images/home/gallery/16.jpeg',
+    '../assets/images/home/gallery/17.jpeg',
+  ];
+
   return (
     <Layout>
       <Breadcumb pageName={'Gallery'} />
       <section>
         <div className="gallery-container">
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/01.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/02.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/03.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/04.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/05.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/06.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/07.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/08.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/09.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/10.jpg" alt="" />
-          </div>
-          <div className="gallery-item">
-            <img src="../assets/images/home/gallery/11.webp" alt="" />
-          </div>
+          {galleryImages.map((imageSrc, index) => (
+            <div className="gallery-item" key={index}>
+              <img src={imageSrc} alt={`Gallery Image ${index + 1}`} />
+            </div>
+          ))}
         </div>
       </section>
     </Layout>
